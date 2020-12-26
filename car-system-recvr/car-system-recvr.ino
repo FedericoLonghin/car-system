52236b0eab5b3b113e747623693735f7be604c61
fededelong03 <65185337+fededelong03@users.noreply.github.com>
2020-12-26 14:26:12 +0100

    Revert "implementing fetch data"
    This reverts commit d6e50cc0a0f1cbdd46273b6f6b207ad2cdee6ac0.

--- car-system-recvr/car-system-recvr.ino
+++ car-system-recvr/car-system-recvr.ino
@@ -2,20 +2,49 @@
 #define SDA 12
 #define SCL 13
 
-short speed, rpm;
-int fuel_level;
-String message;
-void setup() {
-  Serial.begin(250000);
+#define speed 500
+bool prevStat;
+byte result[16];
 
-  Serial.println("ricevitore");
+void setup() {
+  ////Serial.begin(250000);
+
+  //Serial.println("ricevitore");
   pinMode(TR, OUTPUT);
   pinMode(SDA, INPUT);
   pinMode(SCL, INPUT);
-  pinMode(2, INPUT_PULLUP);
+
+
+
+  delay(1000);
+
+  digitalWrite(TR, 1);
+  while (!digitalRead(SCL)) {}
+  delay(100);
+  digitalWrite(TR, 0);
+
+  riceviByte();
+
+String myString = String((char*)result);
+
+Serial.println(myString);
+
 
 }
 void loop() {
-  fetchData();
-  delay(5000);
+
+  
+  //Serial.print("TR: ");
+  //Serial.print(4 + digitalRead(TR));
+
+  //Serial.print("  SDA: ");
+
+
+  //Serial.print(2 + digitalRead(SDA));
+
+  //Serial.print("  SCL: ");
+
+  //Serial.print(digitalRead(SCL));
+  //Serial.println("");
+
 }
